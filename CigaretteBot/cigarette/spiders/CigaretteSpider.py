61e42d2c6f3a035f5fcfc4e88f1b4d2ba9829ee7
gejiqiang <gejiqiang@droi.com>
2018-12-29 16:42:31 +0800

    feature(cigarette):modified the method to import items

--- CigaretteBot/cigarette/spiders/CigaretteSpider.py
+++ CigaretteBot/cigarette/spiders/CigaretteSpider.py
@@ -1,4 +1,7 @@
+#!/usr/bin/env python2
+# -*- encoding=utf-8 -*-
 import scrapy
+from ..items import CigaretteItem
 
 
 class CigaretteSpider(scrapy.Spider):
@@ -17,17 +20,14 @@ class CigaretteSpider(scrapy.Spider):
             if name is None:
                 name = cigarette.xpath('.//div[@class="name"]/h2/b/text()').extract_first();
 
-            if name is None:
-                print(response.body)
+            item = CigaretteItem()
+            item['name'] = name
+            item['category'] = cigarette.xpath('.//div[@class="type"]/p/text()').extract()
+            item['tarContent'] = cigarette.xpath('.//div[@class="tar"]/p/text()').extract()
+            item['price'] = cigarette.xpath('.//div[@class="price"]/p/text()').extract()
+            item['img'] = cigarette.xpath('.//div[@class="img"]/img/@src').extract_first()
+            yield item
 
-            yield {
-                'name': name,
-                'category': cigarette.xpath('.//div[@class="type"]/p/text()').extract(),
-                'tarContent': cigarette.xpath('.//div[@class="tar"]/p/text()').extract(),
-                'price': cigarette.xpath('.//div[@class="price"]/p/text()').extract(),
-                'img': cigarette.xpath('.//div[@class="img"]/img/@src').extract_first(),
-            }
-
-        next_page_url = response.xpath('//a[contains(text(),"下一页")]/@href').extract_first()
+        next_page_url = response.xpath(u'//a[contains(text(),"下一页")]/@href').extract_first()
         if next_page_url is not None:
             yield scrapy.Request(response.urljoin(next_page_url))
--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # ScrapySpider
-Some spider based on scrapy
+
+## 1. 初始化一个SpiderBot
+
+### 1.1 初始化project
+```bash
+scrapy startproject projectname
+```
+
+### 1.2 创建方便执行的入口
+```python
+from scrapy import cmdline
+
+if __name__ == '__main__':
+    cmdline.execute("scrapy crawl cigarette-xpath -o cigarette.json".split())
+```
+
+### 1.3 创建Spider
+```python
+import scrapy
+
+class GiftSpieder(scrapy.Spider):
+    name = "gift-xpath"
+    allowed_domins = "cocodiy.com"
+
+    start_urls = {
+        'https://www.cocodiy.com'
+    }
+
+    def parse(self, response):
+        
+        pass
+```
+
